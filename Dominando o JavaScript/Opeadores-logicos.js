81e1d5b5cb97c72c66b8ee02ef53bb9fbcd5d262
prog-ph <paulodevhenrique@gmail.com>
2023-02-05 13:32:35 -0300

    DevClub

---
+++ Dominando o JavaScript/Opeadores-logicos.js
@@ -0,0 +1,21 @@
+  /*OPERADORES LOGICOS
+
+  && -> E // FILHO OBEDIENTE
+    True && true = true
+    True && false = false
+    false && false = false
+
+
+  || ->  Ou // FILHO ESPERTINHO
+    True || true = true
+    True || false = true
+    false || false = false
+
+
+!   -> Negação // FILHO DO CONTRA / REBELDE
+!   true = false
+    !false = true
+
+*/
+
+console.log(true && false);
---
+++ Dominando o JavaScript/Operadores-ternario.js
@@ -0,0 +1,25 @@
+/* OPERADORES TERNARIO OU CONDICIONAL
+
+?   SE
+:   SE NAO
+*/
+//------- EXEMPLO 01
+// const rain = false
+
+
+// const umbrela = rain ? 'levar guarda-chuva' : 'deixar guarda-chuva'
+
+// console.log(umbrela);
+
+
+
+//-------------EXEMPLO 02
+
+
+// const balance = true
+// const inNotBlocked = true
+// const accountExit = false
+
+// const trasferOk = balance && inNotBlocked && accountExit ? 'TRANSFERENCIA REALIZADA' : 'TRANSFERENCIA NEGADA'
+
+// console.log(trasferOk);
