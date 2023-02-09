995dddbc6523f2ac101e152d403580ddbd09a87c
prog-ph <paulodevhenrique@gmail.com>
2023-02-09 10:02:05 -0300

    Desafio JS DevClub

---
+++ Desafios-JS-DevClub/Desafio-02 - JS01/ex07.js
@@ -0,0 +1,9 @@
+// const number = (Math.floor(Math.random() * (100 - 1) + 1))
+// console.log(number)
+
+
+// if (number % 2 === 0) {
+//     console.log('O numero é PAR')
+// } else {
+//     console.log('O número é IMPAR')
+// }
