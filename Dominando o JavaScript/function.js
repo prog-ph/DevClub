54561d3e73c0ebc6d25dea261ec974705eeacbf8
prog-ph <paulodevhenrique@gmail.com>
2023-02-10 09:27:21 -0300

    FUNÇÕES

---
+++ Dominando o JavaScript/function.js
@@ -0,0 +1,11 @@
+/*
+    FUNÇÃO / FUNCTIONS
+
+
+*/
+
+
+// function printingAboutCodeClub() {
+//     console.log("Estou aprendendo muito com o DevClub");
+// }
+// printingAboutCodeClub()
---
+++ Dominando o JavaScript/function02.js
@@ -0,0 +1,12 @@
+/*
+    FUNÇÃO / FUNCTIONS
+
+    ARGUMENTS AND PARAMETS
+
+*/
+
+function sun(number1, number2) {
+    console.log(number1 - number2);
+    
+}
+sun(2, 4) // INVOCA A FUNÇÃO , CHAMA A FUNÇÃO //
