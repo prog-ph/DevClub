e629e30451b885012906ab4e4cae64e2b62c51af
prog-ph <paulodevhenrique@gmail.com>
2023-02-11 19:36:08 -0300

    DevClub

---
+++ Dominando o JavaScript/Return-Function.js
@@ -0,0 +1,18 @@
+/*
+    FUNÇÃO / FUNCTIONS
+
+    ARGUMENTS AND PARAMETS
+
+*/
+
+function sun(number1,  number2) {
+    const result = number1 + number2
+    return result
+}
+
+const firstNumber = 10
+const seccondNumber = 20
+
+console.log(`A soma dos numeros é ${firstNumber}`);
+console.log(`A soma dos numeros é ${seccondNumber}`);
+console.log(`A soma dos numeros é ${sun(firstNumber, seccondNumber)}`);
--- Dominando o JavaScript/function02.js
+++ Dominando o JavaScript/function02.js
@@ -5,8 +5,8 @@
 
 */
 
-function sun(number1, number2) {
-    console.log(number1 - number2);
+// function sun(number1, number2) {
+//     console.log(number1 - number2);
     
-}
-sun(2, 4) // INVOCA A FUNÇÃO , CHAMA A FUNÇÃO //
+// }
+// sun(2, 4    ) // INVOCA A FUNÇÃO , CHAMA A FUNÇÃO //
