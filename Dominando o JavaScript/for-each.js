e9114d3b27a9b109ab974591f20e647d3ed9bd18
prog-ph <paulodevhenrique@gmail.com>
2023-02-14 09:48:45 -0300

    DevClub

---
+++ Dominando o JavaScript/for-each.js
@@ -0,0 +1,23 @@
+/*
+    FOR EACH
+
+*/
+
+// const students = [
+//     { name: "Paulo", age: 18 },
+//     { name: "Miguel", age: 14 },
+//     { name: "Sarah", age: 11 },
+//     { name: "Edison", age: 37 },
+//     { name: "Janaika", age: 34 },
+// ];
+
+
+// let allStudentsAge = 0
+
+// students.forEach((student, index) => {
+//     allStudentsAge += student.age
+// })
+
+// const averageAge = allStudentsAge / students.length
+
+// console.log(`a media dos alunos e de ${averageAge.toFixed(0)}`);
