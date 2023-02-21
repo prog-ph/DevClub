f2bfd7d35c1f2534913eb41a914308292747b6b2
prog-ph <paulodevhenrique@gmail.com>
2023-02-20 23:44:04 -0300

    DevClub

---
+++ Dominando o JavaScript/Map02.js
@@ -0,0 +1,19 @@
+// const number = [20, 30, 10, 5]
+
+// const students = [
+//     { name: "Paulo", age: 18 },
+//     { name: "Miguel", age: 15 },
+//     { name: "Sarah", age: 12 },
+//     { name: "Pai", age: 37 },
+//     { name: "Mae", age: 34 },
+// ];
+// const double = number => number * 2
+// const sum = number => `R$ ${number.toFixed(2)}`
+ 
+
+// const newArray2 = number.map(double).map(sum)
+// console.log(newArray2);
+
+
+
+
